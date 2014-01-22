9f7ba30a81e178e968b3c01b2e0e6a652c076cf2
Finnur Torfason <finnurtorfa@gmail.com>
2014-01-22 18:14:32 +0100

    Add basic logging functionality

--- db_backup/db_backup.py
+++ db_backup/db_backup.py
@@ -1,55 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine
+import sys, getopt, logging
 
-class DBManager(object):
-  def __init__(self, dialect, dbapi=None, username=None, password=None, addr='localhost',
-               port=None, db=None, echo=False):
-    self.url = self._set_url(dialect=dialect,
-                             dbapi=dbapi,
-                             username=username, 
-                             password=password, 
-                             addr=addr, 
-                             port=port,
-                             db=db)
-    self.engine = create_engine(self.url, echo=echo)
+from os.path import expanduser
 
-  def _set_url(self, **kwargs):
-    """ Returns a string with the database url.
+def init_logging(**kwargs):
+  logging.basicConfig(**kwargs)
+  logging.info('Started running %s', __file__)
 
-    Keyword argument: 
-    dialect     -- string representing the database system to use. 
-    dbapi       -- The dialects DBAPI
-    username    -- The database user
-    password    -- The password of the user
-    addr        -- The IP address to use
-    port        -- The port to use
-    db          -- Name of the database to use
-    """
-    url = kwargs['dialect'] 
-    if kwargs['dbapi']:
-      url = '+'.join([url, kwargs['dbapi']])
-
-    if kwargs['username']:
-      url = '://'.join([url, kwargs['username']])
-
-    if kwargs['password']:
-      url = ':'.join([url, kwargs['password']])
-
-    if kwargs['addr']:
-      url = '@'.join([url, kwargs['addr']])
-
-    if kwargs['port']:
-      url = ':'.join([url, kwargs['port']])
-
-    if kwargs['db']:
-      url = '/'.join([url, kwargs['db']])
-
-    return url
-
-if __name__ == "__main__":
-  bak = DBManager(dialect='postgresql', username='username', password='password',
-          db='db')
-
-__version__ = '0.1'
+if __name__ == '__main__':
+  log_args = {'filename':'db_backup.log', 
+              'format': '[%(levelname)s: %(name)s: %(asctime)s] \t %(message)s', 
+              'datefmt':'%d-%m-%Y %H:%M:%S',
+              'level': 'DEBUG'}  
+  init_logging(**log_args)
+  main(sys.argv[1:])
