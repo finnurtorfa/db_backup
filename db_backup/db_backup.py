58e35962f7cb3d8a3dcc65f2bf137500a4746a4b
Finnur Torfason <finnurtorfa@gmail.com>
2014-01-22 18:35:28 +0100

    improve logging and create 'log' directory if it does not exist. Also add a main function. Now it try's to open a default yml file

--- db_backup/db_backup.py
+++ db_backup/db_backup.py
@@ -3,16 +3,37 @@
 
 import sys, getopt, logging
 
-from os.path import expanduser
+from os import makedirs
+from os.path import expanduser, dirname, exists
 
-def init_logging(**kwargs):
+from manager import DBManager
+
+def main(argv):
+  home = expanduser('~')
+  input_file = home + '/.db_backup.yml'
+
+  try:
+    stream = open(input_file, 'r')
+  except FileNotFoundError:
+    logger.exception('File not found... Exiting!')
+    sys.exit(-1)
+
+def init_logging(dirname='log', **kwargs):
+  if not exists(dirname):
+    makedirs(dirname)
+
   logging.basicConfig(**kwargs)
-  logging.info('Started running %s', __file__)
+  logger = logging.getLogger(__name__)
+  logger.info('Started running %s', __file__)
+  return logger
 
 if __name__ == '__main__':
-  log_args = {'filename':'db_backup.log', 
+  log_args = {'filename':'log/db_backup.log', 
               'format': '[%(levelname)s: %(name)s: %(asctime)s] \t %(message)s', 
               'datefmt':'%d-%m-%Y %H:%M:%S',
               'level': 'DEBUG'}  
-  init_logging(**log_args)
+  logger = init_logging(**log_args)
+  
+  bak = DBManager(dialect='postgresql', username='username', password='password',
+          db='db')
   main(sys.argv[1:])
